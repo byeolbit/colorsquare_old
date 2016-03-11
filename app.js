2e71173c82bbf22c4d5a43555a990398412bf774
starlightcandy <info@byeolbit.com>
2016-03-12 02:54:31 +0900

    Made with Node.js, Socket.io, jQuery and jQuery-UI app.js is server script. index.html client page. Following functions are implemented.
    Connecting
    User list management
    Team list management
    Cell list management
    Change Cell information(Conquer, add stack, attack other's)
    Join to team.
    Generate teams.
     - Validation check for new team name.
     - Refresh new team's color.
    Display current online users.
    Display current user's information.
    Recharge points.(Max 20)
    Display ping's cool time next to cursor.
    Send ping to team.
    Scroll to ping offset, by click ping message link.
    Team chatting.
    Global chatting.
    Disconnecting.

---
+++ app.js
@@ -0,0 +1,563 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('script'));
+app.use(express.static('css'));
+
+var teamList = [];
+var cellInfo = [];
+var colorList = [];
+var userLists = [];
+var teamCount = 0;
+var clients = [];
+var userCount = 0;
+var usedColor = [];
+var stacks = [];
+var scoreList = [];
+
+
+function celldata(c, s, t){
+    this.color = c;
+    this.stack = s;
+    this.team = t;
+    this.isCompleted = false;
+}
+
+function client_class(ID, addr){
+    this.id = ID;
+    this.team = null;
+    this.tname = null;
+    this.nick = newNick();
+    this.point = 0;
+    this.ping = true;
+    this.address = addr;
+}
+
+function getAddr(addr){
+    console.log(addr.address+':'+addr.port);
+    return addr.address+':'+addr.port;
+}
+
+function checkIP(ip){
+    for(var i=0; i<clients.length; i++){
+        if(clients[i].address == ip){
+            return true;
+        }
+    }
+    return false;
+}
+
+function team(teamNum, teamName, teamColor, teamMembers){
+    this.num = teamNum;
+    this.name = teamName;
+    this.color = teamColor;
+    this.members = teamMembers;
+    this.member = [];
+    this.score = 0;
+    usedColor.push(teamColor);
+}
+
+function checkTeamName(newName){
+    for (var i=0; i<teamList.length; i++){
+        if(teamList[i].name == newName){
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkColor(newColor){
+    if(usedColor.indexOf(newColor) == -1){
+        return newColor;
+    }
+    else
+        return -1;
+}
+
+function addPoint(){
+    for (var i=0; i<clients.length; i++){
+        if(clients[i].team!=null){
+            clients[i].point = addpnt(clients[i].point);
+            io.sockets.connected[clients[i].id].emit('return userPoint',clients[i].point);
+        }
+    }
+}
+
+function setPing(id){
+    i = findClientIndex(id);
+    clients[i].ping = false;
+    setTimeout(function(){clients[i].ping = true;}, 20000);
+}
+
+function addpnt(point){
+    if(point<20) {point++;}
+    return point;
+}
+
+function calScore(teamNum, scorePoint){
+    teamList[teamNum].score += scorePoint;
+    io.emit("refresh teamScore");
+}
+
+function findTeamIndex(teamNum){
+    for (var i=0; i<teamList.length; i++){
+        if(teamList[i].num == teamNum){
+            return i;
+        }
+    }
+}
+
+function findByName(teamName){
+    for (var i=0; i<teamList.length; i++){
+        if(teamList[i].name == teamName){
+            return i;
+        }
+    }
+}
+
+function stackEffect(cellNum, refColor){
+
+    var leftNum = cellNum-1;
+    var rightNum = cellNum+1;
+    var downNum = cellNum+40;
+    var upNum = cellNum-40;
+
+    var leftSide = (cellNum%40)==1;
+    var upSide = (cellNum-40)<1;
+    var rightSide = (cellNum%40)==0;
+    var downSide = (cellNum+40)>1600;
+
+    if(leftSide && upSide){
+        leftNum = cellNum+39;
+        upNum = cellNum+1560;
+        //console.log('catch1');
+    }
+    else if(upSide && rightSide){
+        upNum = cellNum+1560;
+        rightNum = cellNum-39;
+        //console.log('catch2');
+    }
+    else if(rightSide && downSide){
+        rightNum = cellNum-39;
+        downNum = cellNum-1560;
+        //console.log('catch3');
+    }
+
+    else if(downSide && leftSide){
+        downNum = cellNum-1560;
+        leftNum = cellNum+39;
+        //console.log('catch4');
+    }
+
+    else if(upSide){
+        upNum = cellNum+1560;
+        //console.log('catch5');
+    }
+
+    else if(rightSide){
+        rightNum = cellNum-39;
+        //console.log('catch6');
+    }
+
+    else if(downSide){
+        downNum = cellNum-1560;
+        //console.log('catch7');
+    }
+
+    else if(leftSide){
+        leftNum = cellNum+39;
+        //console.log('catch8');
+    }
+
+    if((cellInfo[rightNum].color==refColor) && (cellInfo[downNum].color==refColor) && (cellInfo[leftNum].color==refColor) && (cellInfo[upNum].color==refColor)){
+        if(!cellInfo[cellNum].isCompleted){
+            cellInfo[cellNum].stack++;
+            cellInfo[cellNum].isCompleted = true;
+            //console.log('break one : ' + cellInfo[cellNum].stack +'/' +cellInfo[cellNum].isCompleted);//debug
+
+        }
+    }
+}
+for (var i=1; i<1601; i++){
+    cellInfo[i] = new celldata("#AAAAAA",0,null);
+    colorList[i] = "#AAAAAA";
+    stacks[i] = 0;
+}
+var pointTimer = setInterval(addPoint, 1000);
+
+function newNick(){
+     return 'user#'+(Math.random() + 1).toString(36).substr(6);
+}
+
+function compare(a,b) {
+    if (a.score < b.score)
+        return 1;
+    else if (a.score > b.score)
+        return -1;
+    else
+        return 0;
+}
+
+function findTeambyID(userid) {
+    for (var i=0; i<clients.length; i++){
+        if(clients[i].id == userid){
+            return clients[i].team;
+        }
+    }
+}
+
+function findClientIndex(userid){
+    for (var i=0; i<clients.length; i++){
+        if(clients[i].id == userid){
+            return i;
+        }
+    }
+}
+
+function joinTeam(sckt,teamname){
+    var t = teamList[findByName(teamname)];
+    t.members++;
+    t.member.push(sckt.id);
+    clients[findClientIndex(sckt.id)].team = t.num;
+    clients[findClientIndex(sckt.id)].tname = t.name;
+    io.sockets.connected[sckt.id].emit('joined',t.name,t.color,t.num);
+    io.sockets.connected[sckt.id].emit('team score',t.score);
+    sckt.join(t.name);
+    io.to(teamname).emit('team memeber changed', t.members);
+}
+
+function sortScoreBoard(){
+    teamList.sort(compare);
+    for(var i=0;i<teamList.length; i++){
+      scoreList[i] = (teamList[i].name+'('+teamList[i].score+')');
+    }
+    io.emit('change score', scoreList.splice(0,20));
+}
+
+teamList.push(new team(teamCount++, 'Red', '#cd2727', 0));
+teamList.push(new team(teamCount++, 'blue', '#2c2cbf', 0));
+teamList.push(new team(teamCount++, 'green', '#47c347', 0));
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+
+    //var addr = socket.request.connection.remoteAddress;
+    var ip = socket.request.headers['x-forwarded-for'] ||
+     socket.req.connection.remoteAddress ||
+     socket.req.socket.remoteAddress ||
+     socket.req.connection.socket.remoteAddress;
+    console.log(ip);
+    /*
+    if(checkIP(addr)){
+        io.sockets.connected[socket.id].emit('wrong connectoin');
+        socket.disconnect();
+        return false;
+    }
+    */
+    clients.push(new client_class(socket.id, addr));
+
+    userCount++;
+    console.log(userCount+clients[userCount-1].id);
+    io.sockets.connected[socket.id].emit('new user', clients[userCount-1].nick);
+    io.emit('online users',userCount);
+    sortScoreBoard();
+
+    /*
+    socket.on('use', function(rID){
+        console.log('recieved : '+rID);
+    });
+    */
+
+    socket.on('request init', function(){
+        io.sockets.connected[socket.id].emit('load colors', colorList, stacks);
+        io.sockets.connected[socket.id].emit('load teams', teamList);
+    });
+
+
+    socket.on('change cell', function(className){
+        var clt = clients[findClientIndex(socket.id)];
+        var newColor = teamList[findTeamIndex(clt.team)].color;
+        var teamName = teamList[findTeamIndex(clt.team)].name;
+        var teamNum = teamList[findTeamIndex(clt.team)].num;
+        var cellNum = className.substr(5)*1;
+        var stackChangedCell = [];
+        var updown = []; //1 is stack pushed, 0 is no change, -1 is stack poped
+
+        for(var i=0; i<5; i++){
+            updown[i] = 0;
+        }
+
+        var leftNum = cellNum-1;
+        var rightNum = cellNum+1;
+        var downNum = cellNum+40;
+        var upNum = cellNum-40;
+
+        var leftSide = (cellNum%40)==1;
+        var upSide = (cellNum-40)<1;
+        var rightSide = (cellNum%40)==0;
+        var downSide = (cellNum+40)>1600;
+
+        if(leftSide && upSide){
+            leftNum = cellNum+39;
+            upNum = cellNum+1560;
+            //console.log('catch1');
+        }
+        else if(upSide && rightSide){
+            upNum = cellNum+1560;
+            rightNum = cellNum-39;
+            //console.log('catch2');
+        }
+        else if(rightSide && downSide){
+            rightNum = cellNum-39;
+            downNum = cellNum-1560;
+            //console.log('catch3');
+        }
+
+        else if(downSide && leftSide){
+            downNum = cellNum-1560;
+            leftNum = cellNum+39;
+            //console.log('catch4');
+        }
+
+        else if(upSide){
+            upNum = cellNum+1560;
+            //console.log('catch5');
+        }
+
+        else if(rightSide){
+            rightNum = cellNum-39;
+            //console.log('catch6');
+        }
+
+        else if(downSide){
+            downNum = cellNum-1560;
+            //console.log('catch7');
+        }
+
+        else if(leftSide){
+            leftNum = cellNum+39;
+            //console.log('catch8');
+        }
+
+
+        //console.log(cellNum);
+
+        var teamOfCell = teamList[findTeamIndex(cellInfo[cellNum].team)];
+        var teamOfLCell = teamList[findTeamIndex(cellInfo[leftNum].team)];
+        var teamOfRCell = teamList[findTeamIndex(cellInfo[rightNum].team)];
+        var teamOfUCell = teamList[findTeamIndex(cellInfo[upNum].team)];
+        var teamOfDCell = teamList[findTeamIndex(cellInfo[downNum].team)];
+
+        if(cellInfo[cellNum].stack > 0){
+            /* When cell is already colored */
+            if(cellInfo[cellNum].color == newColor){
+                /* If requested color is match with target cell's color  */
+                cellInfo[cellNum].stack++;
+                updown[0] = 1;
+                teamOfCell.score++;
+                clt.point--;
+            }
+            else{
+                /* If requested color isn't matched with target cell's color
+                 * Subtract 1 from target cell's stack */
+                if(clt.point<1){
+                    return false;
+                }
+                if(cellInfo[cellNum].isCompleted){
+                    if(clt.point<2){
+                        return false;
+                    }
+                    cellInfo[cellNum].stack--;
+                    updown[0] = -1;
+                    teamOfCell.score--;
+                    clt.point -= 2;
+                }
+                else{
+                    cellInfo[cellNum].stack--;
+                    updown[0] = -1;
+                    teamOfCell.score--;
+                    clt.point--;
+                }
+
+                if(cellInfo[cellNum].stack == 0){
+                    /* When cell's stack becomes 0, init the cell's data */
+                    cellInfo[cellNum].color="#AAAAAA";
+                    cellInfo[cellNum].team=null;
+                    cellInfo[cellNum].isCompleted=false;
+
+                    /* This checks around cell and change the state */
+                    if(cellInfo[leftNum].isCompleted){
+                        cellInfo[leftNum].isCompleted=false;
+                    }
+                    if(cellInfo[rightNum].isCompleted){
+                        cellInfo[rightNum].isCompleted=false;
+                    }
+                    if(cellInfo[upNum].isCompleted){
+                        cellInfo[upNum].isCompleted=false;
+                    }
+                    if(cellInfo[downNum].isCompleted){
+                        cellInfo[downNum].isCompleted=false;
+                    }
+                }
+
+            }
+        }
+
+        else{
+            cellInfo[cellNum].color = newColor;
+            cellInfo[cellNum].stack = 1;
+            cellInfo[cellNum].team = teamNum;
+            updown[0] = 1;
+            teamOfCell = teamList[findTeamIndex(cellInfo[cellNum].team)];
+            teamOfCell.score++;
+
+            stackEffect(cellNum, newColor);
+
+            clt.point--;
+
+            if(cellInfo[leftNum].color == newColor){
+                var oldstack = stacks[leftNum];
+                stackEffect(leftNum, newColor);
+                stacks[leftNum] = cellInfo[leftNum].stack;
+                if (oldstack<stacks[leftNum]){
+                    updown[1] = 1;
+                    teamOfLCell.score++;
+
+                }
+            }
+            if(cellInfo[rightNum].color == newColor){
+                var oldstack = stacks[rightNum];
+                stackEffect(rightNum, newColor);
+                stacks[rightNum] = cellInfo[rightNum].stack;
+                if (oldstack<stacks[rightNum]){
+                    updown[2] = 1;
+                    teamOfRCell.score++;
+                }
+            }
+            if(cellInfo[upNum].color == newColor){
+                var oldstack = stacks[upNum];
+                stackEffect(upNum, newColor);
+                stacks[upNum] = cellInfo[upNum].stack;
+                if (oldstack<stacks[upNum]){
+                    updown[3] = 1;
+                    teamOfUCell.score++;
+
+                }
+            }
+            if(cellInfo[downNum].color == newColor){
+                var oldstack = stacks[downNum];
+                stackEffect(downNum, newColor);
+                stacks[downNum] = cellInfo[downNum].stack;
+                if (oldstack<stacks[downNum]){
+                    updown[4] = 1;
+                    teamOfDCell.score++;
+                }
+            }
+        }
+
+        colorList[cellNum] = cellInfo[cellNum].color;
+        stacks[cellNum] = cellInfo[cellNum].stack;
+
+        var new_className = [];
+
+        new_className[0] = '.'+className.substr(0,4)+'.'+cellNum;
+        new_className[1] = '.'+className.substr(0,4)+'.'+leftNum;
+        new_className[2] = '.'+className.substr(0,4)+'.'+rightNum;
+        new_className[3] = '.'+className.substr(0,4)+'.'+upNum;
+        new_className[4] = '.'+className.substr(0,4)+'.'+downNum;
+
+        var changedStack = [cellInfo[cellNum].stack, cellInfo[leftNum].stack, cellInfo[rightNum].stack, cellInfo[upNum].stack, cellInfo[downNum].stack];
+        var cellColor = [colorList[cellNum], colorList[leftNum], colorList[rightNum], colorList[upNum], colorList[downNum]];
+
+        sortScoreBoard();
+
+        io.emit('change cell', cellColor, new_className, changedStack, updown);
+        io.sockets.connected[socket.id].emit('return userPoint', clt.point);
+        io.to(teamList[findTeamIndex(teamNum)].name).emit('team score', teamList[findTeamIndex(teamNum)].score);
+    });
+
+    socket.on('newColor check',function(newColor){
+        io.sockets.connected[socket.id].emit('color check result',checkColor(newColor));
+
+    });
+
+    socket.on('gen team', function(tN, tC){
+        var isGen = false;
+        var teamNum;
+        if(checkTeamName(tN)){
+            teamCount++;
+            teamNum = teamCount;
+            teamList.push(new team(teamNum, tN, tC, 1));
+            isGen = true;
+            joinTeam(socket,tN)
+
+        }
+        console.log(tN + ' generated')
+        io.sockets.connected[socket.id].emit('gen result',isGen);
+
+    });
+
+
+    socket.on('join team', function(teamname){
+        joinTeam(socket,teamname);
+        io.sockets.connected[socket.id].emit('global msg', 'Welcome to C0L0R SQ@RE. If you have bug report or opinion please send me an email : info@byeolbit.com  Have a good time!', 'System');
+    });
+
+    socket.on('send team msg', function(msg){
+        if (msg==''){
+            return false;
+        }
+        var c = clients[findClientIndex(socket.id)];
+        msg = c.nick + ':' + msg;
+        io.to(c.tname).emit('team msg', msg, c.nick);
+    });
+
+    socket.on('send global msg', function(msg){
+        if (msg==''){
+            return false;
+        }
+        var c = clients[findClientIndex(socket.id)];
+        msg = c.nick + ':' + msg;
+        io.emit('global msg', msg, c.nick);
+
+    });
+
+    socket.on('request ping', function(e){
+        var clnt = clients[findClientIndex(socket.id)];
+        var ping = clnt.ping;
+        if(clnt.ping == true){
+            setPing(clnt.id);
+            var x = e.pageX;
+            var y = e.pageY;
+            var cooridm = +x+'m'+y;
+            var cooridp = +x+'p'+y;
+            var str = 'Support request on x:'+x+' / y:'+y;
+            var fun = '<a href="#" onclick="scrollScreen(event,'+x+','+y+');return false;">'+str+'</a>';
+            io.to(clnt.tname).emit('ping msg', fun);
+            io.to(clnt.tname).emit('send ping',cooridp, cooridm, x, y);
+        }
+    });
+
+    socket.on('disconnect', function(){
+        console.log(socket.id + 'user disconnected');
+        if(clients[findClientIndex(socket.id)].team != null){
+            var t = teamList[findTeamIndex(findTeambyID(socket.id))];
+            t.member.splice(t.member.indexOf(socket.id),1);
+            t.members--;
+            io.to(t.name).emit('team memeber changed', t.members);
+        }
+        clients.splice(findClientIndex(socket.id),1);
+        userCount--;
+        io.emit('online users',userCount);
+    });
+
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
---
+++ script/script.js
@@ -0,0 +1,317 @@
+var socket = io();
+var userColor=null;
+var userNick=null;
+var userTeam=null;
+var userPoint = 0;
+var tempColor=null;
+var teamNum=null;
+var userPing=true;
+
+var safeColors = ['00','33','66','99','cc','ff'];
+var rand = function() {
+    return Math.floor(Math.random()*6);
+}
+var randomColor = function() {
+    var r = safeColors[rand()];
+    var g = safeColors[rand()];
+    var b = safeColors[rand()];
+    return "#"+r+g+b;
+}
+// random color generator by mtf
+
+var setUserInfo = function(){
+    $('.currentTeam').text(userTeam);
+    $('.currentColor').css('background-color',userColor);
+    //socket.emit('request points',userID);
+}
+
+var updatePoint = function(point){
+    for(i=1;i<=point;i++){
+        var cell = '.pointCell:nth-child('+i+')';
+        $(cell).addClass('ready');
+    }
+    for(i=point+1;i<=20;i++){
+        var cell = '.pointCell:nth-child('+i+')';
+        $(cell).removeClass('ready');
+    }
+    userPoint = point;
+    //$('.currentPoint').html(userPoint);
+}
+
+function scrollScreen(event,tx,ty){
+    var x,y;
+    x = event.pageX-100;
+    y = event.pageY-200;
+    $('html, body').animate({ scrollLeft:tx-x, scrollTop:ty-y },2000,'easeInOutQuint');
+    //$('html, body').animate({},500);
+    //window.scrollBy(tx-x, ty-y);
+}
+
+var pTimer;
+
+function pingCal(){
+    var i =20.0;
+    function p(){
+        i -= 0.1;
+        $('.pingInfo p').text('Ping : '+i.toFixed(1)+' sec left');
+    }
+    pTimer = setInterval(p,100);
+}
+
+$(document).ready(function () {
+
+    tempColor=randomColor();
+
+    for(var i=1; i<1601; i++){
+        $('.cellField').append('<div class = "cell '+i+'"></div>');
+    }
+
+    for(var i=1; i<21; i++){
+        $('.currentPoint').append('<div class = "pointCell '+i+'"></div>');
+    }
+    socket.on('new user',function(nick){
+            userNick = nick;
+            socket.emit('request init');
+            //console.log('client : '+userID);
+            //socket.emit('use',userID);
+            //for debug
+    });
+
+    /* This is for ping menu */
+
+    document.oncontextmenu = function() {return false;};
+
+    $(document).on('mousemove',function(e){
+        $('.pingInfo').offset({top:e.pageY+10, left:e.pageX+10});
+    });
+
+    $('.popup .make_team .contents .colorbox').css('background-color',tempColor);
+
+
+    $('.popup .make_team button#genteam').prop('disabled',true);
+
+
+    $('li.pingli').click(function(e){
+        var ox = $(this).text.splice(22,4);
+        alert(ox);
+    });
+
+
+    $('.cellField .cell').bind('mousedown', function(e){
+        if(e.button==0){
+            if(userPoint>0){
+                socket.emit('change cell',e.target.className);
+                return false;
+            }
+        }
+        else{
+            if(userTeam != null && userPing){
+                var eventData = function(e){
+                    this.pageX = e.pageX;
+                    this.pageY = e.pageY;
+                }
+                socket.emit('request ping',new eventData(e));
+                return false;
+            }
+        }
+    });
+
+    socket.on('return userPoint',function(point){
+        updatePoint(point);
+    });
+
+    /* Join to team */
+    $('.popup .team_select button').click(function(){
+        socket.emit('join team', $('.popup .team_select select#teamlists option:selected').text());
+    });
+
+    socket.on('joined',function(teamName, teamColor, teamNumber){
+        userColor = teamColor;
+        userTeam = teamName;
+        teamNum = teamNumber;
+        $('.overlay').fadeOut();
+        $('.popup').fadeOut();
+        setUserInfo();
+        $('body').append('');
+        $('.pingInfo').css('visibility','visible');
+    });
+
+    $('.popup .make_team input#new_team_name').on('input', function(){
+
+        var inputbox=$(this);
+        //var inputVal=$(this).val();
+        var inputLen=$(this).val().length;
+
+        //console.log(input); //for debug
+
+        if(inputLen > 0 && inputLen < 25){
+            inputbox.removeClass('invalid').addClass('valid');
+            $('.popup .make_team button#genteam').prop('disabled',false);
+        }
+        else{
+            inputbox.removeClass('valid').addClass('invalid');
+            $('.popup .make_team button#genteam').prop('disabled',true);
+        }
+    });
+
+    /* Refresh team color */
+    $('.popup .make_team button#refresh').click(function(){
+        socket.emit('newColor check',randomColor());
+    });
+
+    socket.on('color check result',function(result){
+        if(result != -1){
+            tempColor = result;
+            $('.popup .make_team .contents .colorbox').css('background-color',tempColor);
+        }
+        else{
+             socekt.emit('newColor check',tempColor);
+        }
+    });
+
+    /* Generate team */
+    $('.popup .make_team button#genteam').click(function(){
+        socket.emit('gen team',$('.popup .make_team input#new_team_name').val(),tempColor);
+    });
+
+
+    socket.on('gen result',function(isGen){
+        if(!isGen){
+            alert('The team name is already exist!');
+        }
+    });
+
+    /* Cell color changing */
+    socket.on('change cell', function(colora, className, stack, isChanged){
+        for(var i=0; i<5; i++){
+            $(className[i]).css('background-color', colora[i]);
+            if(stack[i]!=0){
+                $(className[i]).text(stack[i]);
+            }
+            else{
+                $(className[i]).text('　');
+            }
+            if(isChanged[i]==-1){
+                $(className[i]).effect('bounce',{times:3}, 150);
+            }
+
+        }
+
+    });
+
+    socket.on('load colors', function(cellColor, stacks){
+        for(i=1; i<1601; i++){
+            var cellName = '.cellField .cell.'+i;
+            $(cellName).css('background-color',cellColor[i]);
+            if(stacks[i]!=0){
+                $(cellName).text(stacks[i]);
+            }
+            else{
+                $(cellName).text('　');
+            }
+        }
+    });
+
+
+    socket.on('load teams', function(teamList){
+        var sel = $('<select id="teamlists" title="select_team">').appendTo('.popup .team_select label');
+        $(teamList).each(function(){
+            sel.append($("<option>").text(this.name));
+        });
+    });
+
+
+    socket.on('change score', function(list){
+      $('#slist').html('');
+      $(list).each(function(){
+          $('#slist').append($('<li>').text(this));
+      });
+    });
+
+    socket.on('team score', function(score){
+      $('#score').text(score);
+    });
+
+    socket.on('team memeber changed', function(teamMem){
+      $('#mN').text(teamMem);
+    });
+
+    /* team chat */
+    $('#t').on('keypress', function(event){
+        if(event.keyCode == 13 && $('#t').val()!=''){
+            socket.emit('send team msg',$('#t').val());
+            $('#t').val('');
+        }
+    });
+
+    $('#ts').click(function(){
+            socket.emit('send team msg',$('#t').val());
+            $('#t').val('');
+    });
+
+    /* global chat */
+    $('#g').on('keypress', function(event){
+        if(event.keyCode == 13){
+            socket.emit('send global msg',$('#g').val());
+            $('#g').val('');
+        }
+    });
+
+    $('#gs').click(function(){
+            socket.emit('send global msg',$('#g').val());
+            $('#g').val('');
+    });
+
+    socket.on('team msg',function(msg, nick){
+        var lis;
+        if(nick == userNick){
+            lis = '<li class="you">'
+        }
+        else{
+            lis = '<li>';
+        }
+        $('#ct').append($(lis).text(msg));
+        $('#ct').animate({scrollTop: $('#ct')[0].scrollHeight});
+    });
+
+    socket.on('global msg', function(msg, nick){
+        var lis;
+        if(nick == userNick){
+            lis = '<li class="you">'
+        }
+        else{
+            lis = '<li>';
+        }
+        $('#cg').append($(lis).text(msg));
+        $('#cg').animate({scrollTop: $('#cg')[0].scrollHeight});
+    });
+
+    socket.on('send ping', function(cdp, cdm, x, y){
+        var wID=x+'i'+y;
+        y-=8;
+        x-=8;
+        $('body').append('<div class = "markerWrap" id="'+wID+'" style="top:'+(y)+'px; left:'+x+'px;"><div class="marker" id ="'+cdm+'"></div><div class="pulse" id ="'+cdp+'"></div></div>');
+        $('#'+wID).effect('bounce',{times:4},700);
+        userPing = false;
+        pingCal();
+        setTimeout(function(){
+            $('#'+cdp).fadeOut("slow");
+            $('#'+cdm).fadeOut("slow");
+            $('#'+wID).remove();
+            setTimeout(function(){
+                clearInterval(pTimer);
+                $('.pingInfo p').text('Ping : ready');
+                userPing = true;}, 10000);
+            },10000);
+        });
+
+      socket.on('ping msg', function(msg){
+          $('#ct').append($('<li class="pingli">').html("&nbsp;System : "+msg+" (click to move)"));
+          $('#ct').animate({scrollTop: $('#ct')[0].scrollHeight});
+      });
+
+      socket.on('online users', function(n){
+          $('.userInfo p').text('users online : '+n);
+      })
+
+});
